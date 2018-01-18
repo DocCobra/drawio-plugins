7389e64514f40d357f3568d1f949d91d946ef5b7
DocCobra <artuthebest@gmail.com>
2018-01-18 10:02:05 +0100

    Create big-button.js

---
+++ big-button.js
@@ -0,0 +1,6 @@
+Draw.loadPlugin(function(ui) {
+  ui.editor.graph.collapsedImage.width = 30;
+  ui.editor.graph.collapsedImage.height = 30;
+  ui.editor.graph.expandedImage.width = 30;
+  ui.editor.graph.expandedImage.height = 30;
+});
